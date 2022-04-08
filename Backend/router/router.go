205d88e7751ed9e8a6f0989f0eb7e2c54951b996
mayichong <mayichong123@gmail.com>
2022-04-07 20:10:22 -0400

    initialize backend

--- Backend/router/router.go
+++ Backend/router/router.go
@@ -8,15 +8,26 @@ import (
 
 func RunServer() {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
+	r.GET("/v1/status", statusCheck)
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	fmt.Println("Server is running on port 4000...")
+	r.Run(":4000")
+}
 
-	r.GET("/status", r.statusHandler)
+//CORS error fix
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
-	fmt.Println("Hello!")
-	r.Run(":8080")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
